572a4862b6f3099b99c9e1e158906a9aa9a22440
pereyra-m <matias.pereyra@wazuh.com>
2020-12-29 23:51:43 -0300

    Updating update.sh with WAZUH_HOME variable

--- src/init/update.sh
+++ src/init/update.sh
@@ -12,12 +12,12 @@ isUpdate()
     ls -la ${OSSEC_INIT} > /dev/null 2>&1
     if [ $? = 0 ]; then
         . ${OSSEC_INIT}
-        if [ "X$DIRECTORY" = "X" ]; then
+        if [ "X$WAZUH_HOME" = "X" ]; then
             echo "# ($FUNCNAME) ERROR: The variable DIRECTORY wasn't set" 1>&2
             echo "${FALSE}"
             return 1;
         fi
-        ls -la $DIRECTORY > /dev/null 2>&1
+        ls -la $WAZUH_HOME > /dev/null 2>&1
         if [ $? = 0 ]; then
             echo "${TRUE}"
             return 0;
@@ -31,7 +31,7 @@ doUpdatecleanup()
 {
     . ${OSSEC_INIT}
 
-    if [ "X$DIRECTORY" = "X" ]; then
+    if [ "X$WAZUH_HOME" = "X" ]; then
         echo "# ($FUNCNAME) ERROR: The variable DIRECTORY wasn't set." 1>&2
         echo "${FALSE}"
         return 1;
@@ -39,9 +39,9 @@ doUpdatecleanup()
 
     # Checking if the directory is valid.
     _dir_pattern_update="^/[-a-zA-Z0-9/\.-]{3,128}$"
-    echo $DIRECTORY | grep -E "$_dir_pattern_update" > /dev/null 2>&1
+    echo $WAZUH_HOME | grep -E "$_dir_pattern_update" > /dev/null 2>&1
     if [ ! $? = 0 ]; then
-        echo "# ($FUNCNAME) ERROR: directory name ($DIRECTORY) doesn't match the pattern $_dir_pattern_update" 1>&2
+        echo "# ($FUNCNAME) ERROR: directory name ($WAZUH_HOME) doesn't match the pattern $_dir_pattern_update" 1>&2
         echo "${FALSE}"
         return 1;
     fi
@@ -58,7 +58,7 @@ getPreinstalled()
 getPreinstalledDir()
 {
     . ${OSSEC_INIT}
-    echo "$DIRECTORY"
+    echo "$WAZUH_HOME"
     return 0;
 }
 
@@ -88,7 +88,7 @@ UpdateStartOSSEC()
    elif [ `stat /proc/1/exe 2> /dev/null | grep "init.d" | wc -l` -ne 0 ]; then
        service wazuh-$TYPE start
    else
-       $DIRECTORY/bin/wazuh-control start
+       $WAZUH_HOME/bin/wazuh-control start
    fi
 }
 
@@ -118,26 +118,26 @@ UpdateStopOSSEC()
     fi
 
     # Make sure Wazuh is stopped
-    $DIRECTORY/bin/wazuh-control stop > /dev/null 2>&1
+    $WAZUH_HOME/bin/wazuh-control stop > /dev/null 2>&1
     sleep 2
 
    # We also need to remove all syscheck queue file (format changed)
     if [ "X$VERSION" = "X0.9-3" ]; then
-        rm -f $DIRECTORY/queue/syscheck/* > /dev/null 2>&1
-        rm -f $DIRECTORY/queue/agent-info/* > /dev/null 2>&1
+        rm -f $WAZUH_HOME/queue/syscheck/* > /dev/null 2>&1
+        rm -f $WAZUH_HOME/queue/agent-info/* > /dev/null 2>&1
     fi
-    rm -rf $DIRECTORY/framework/* > /dev/null 2>&1
-    rm $DIRECTORY/wodles/aws/aws > /dev/null 2>&1 # this script has been renamed
-    rm $DIRECTORY/wodles/aws/aws.py > /dev/null 2>&1 # this script has been renamed
+    rm -rf $WAZUH_HOME/framework/* > /dev/null 2>&1
+    rm $WAZUH_HOME/wodles/aws/aws > /dev/null 2>&1 # this script has been renamed
+    rm $WAZUH_HOME/wodles/aws/aws.py > /dev/null 2>&1 # this script has been renamed
 
     # Deleting plain-text agent information if exists (it was migrated to Wazuh DB in v4.1)
-    if [ -d "$DIRECTORY/queue/agent-info" ]; then
-        rm -rf $DIRECTORY/queue/agent-info > /dev/null 2>&1
+    if [ -d "$WAZUH_HOME/queue/agent-info" ]; then
+        rm -rf $WAZUH_HOME/queue/agent-info > /dev/null 2>&1
     fi
 
     # Deleting plain-text rootcheck information if exists (it was migrated to Wazuh DB in v4.1)
-    if [ -d "$DIRECTORY/queue/rootcheck" ]; then
-        rm -rf $DIRECTORY/queue/rootcheck > /dev/null 2>&1
+    if [ -d "$WAZUH_HOME/queue/rootcheck" ]; then
+        rm -rf $WAZUH_HOME/queue/rootcheck > /dev/null 2>&1
     fi
 }
 
@@ -161,8 +161,8 @@ UpdateOldVersions()
         return
     fi
 
-    OSSEC_CONF_FILE="$DIRECTORY/etc/ossec.conf"
-    OSSEC_CONF_FILE_ORIG="$DIRECTORY/etc/ossec.conf.orig"
+    OSSEC_CONF_FILE="$WAZUH_HOME/etc/ossec.conf"
+    OSSEC_CONF_FILE_ORIG="$WAZUH_HOME/etc/ossec.conf.orig"
 
     # ossec.conf -> ossec.conf.orig
     cp -pr $OSSEC_CONF_FILE $OSSEC_CONF_FILE_ORIG
@@ -175,30 +175,30 @@ UpdateOldVersions()
     if [ ! "$INSTYPE" = "agent" ]; then
 
         # Delete old update ruleset
-        if [ -d "$DIRECTORY/update" ]; then
-            rm -rf "$DIRECTORY/update"
+        if [ -d "$WAZUH_HOME/update" ]; then
+            rm -rf "$WAZUH_HOME/update"
         fi
 
-        ETC_DECODERS="$DIRECTORY/etc/decoders"
-        ETC_RULES="$DIRECTORY/etc/rules"
+        ETC_DECODERS="$WAZUH_HOME/etc/decoders"
+        ETC_RULES="$WAZUH_HOME/etc/rules"
 
         # Moving local_decoder
-        if [ -f "$DIRECTORY/etc/local_decoder.xml" ]; then
-            if [ -s "$DIRECTORY/etc/local_decoder.xml" ]; then
-                mv "$DIRECTORY/etc/local_decoder.xml" $ETC_DECODERS
+        if [ -f "$WAZUH_HOME/etc/local_decoder.xml" ]; then
+            if [ -s "$WAZUH_HOME/etc/local_decoder.xml" ]; then
+                mv "$WAZUH_HOME/etc/local_decoder.xml" $ETC_DECODERS
             else
                 # it is empty
-                rm -f "$DIRECTORY/etc/local_decoder.xml"
+                rm -f "$WAZUH_HOME/etc/local_decoder.xml"
             fi
         fi
 
         # Moving local_rules
-        if [ -f "$DIRECTORY/rules/local_rules.xml" ]; then
-            mv "$DIRECTORY/rules/local_rules.xml" $ETC_RULES
+        if [ -f "$WAZUH_HOME/rules/local_rules.xml" ]; then
+            mv "$WAZUH_HOME/rules/local_rules.xml" $ETC_RULES
         fi
 
         # Creating backup directory
-        BACKUP_RULESET="$DIRECTORY/etc/backup_ruleset"
+        BACKUP_RULESET="$WAZUH_HOME/etc/backup_ruleset"
         mkdir $BACKUP_RULESET > /dev/null 2>&1
         chmod 750 $BACKUP_RULESET > /dev/null 2>&1
         chown root:ossec $BACKUP_RULESET > /dev/null 2>&1
@@ -207,27 +207,27 @@ UpdateOldVersions()
         old_decoders="ossec_decoders wazuh_decoders"
         for old_decoder in $old_decoders
         do
-            if [ -d "$DIRECTORY/etc/$old_decoder" ]; then
-                mv "$DIRECTORY/etc/$old_decoder" $BACKUP_RULESET
+            if [ -d "$WAZUH_HOME/etc/$old_decoder" ]; then
+                mv "$WAZUH_HOME/etc/$old_decoder" $BACKUP_RULESET
             fi
         done
 
         # Backup decoders: Wazuh v1.0 and OSSEC
-        if [ -f "$DIRECTORY/etc/decoder.xml" ]; then
-            mv "$DIRECTORY/etc/decoder.xml" $BACKUP_RULESET
+        if [ -f "$WAZUH_HOME/etc/decoder.xml" ]; then
+            mv "$WAZUH_HOME/etc/decoder.xml" $BACKUP_RULESET
         fi
 
         # Backup rules: All versions
-        mv "$DIRECTORY/rules" $BACKUP_RULESET
+        mv "$WAZUH_HOME/rules" $BACKUP_RULESET
 
         # New ossec.conf by default
         ./gen_ossec.sh conf "manager" $DIST_NAME $DIST_VER > $OSSEC_CONF_FILE
-        ./add_localfiles.sh $DIRECTORY >> $OSSEC_CONF_FILE
+        ./add_localfiles.sh $WAZUH_HOME >> $OSSEC_CONF_FILE
     else
         # New ossec.conf by default
         ./gen_ossec.sh conf "agent" $DIST_NAME $DIST_VER > $OSSEC_CONF_FILE
         # Replace IP
         ./src/init/replace_manager_ip.sh $OSSEC_CONF_FILE_ORIG $OSSEC_CONF_FILE
-        ./add_localfiles.sh $DIRECTORY >> $OSSEC_CONF_FILE
+        ./add_localfiles.sh $WAZUH_HOME >> $OSSEC_CONF_FILE
     fi
 }
